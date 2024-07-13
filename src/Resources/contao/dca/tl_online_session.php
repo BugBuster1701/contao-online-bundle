dce166760d6b954a391f2b3d8a6f1d86e120f5c3
Glen Langer <contao.modul@glen-langer.de>
2024-07-13 15:47:56 +0200

    # 13, Aufruf ecs-legacy

--- src/Resources/contao/dca/tl_online_session.php
+++ src/Resources/contao/dca/tl_online_session.php
@@ -1,46 +1,46 @@
 <?php
 
 /*
- * This file is part of a BugBuster Contao Bundle
+ * This file is part of a BugBuster Contao Bundle.
  *
- * @copyright  Glen Langer 2020 <http://contao.ninja>
+ * @copyright  Glen Langer 2024 <http://contao.ninja>
  * @author     Glen Langer (BugBuster)
- * @package    Online-Bundle
+ * @package    Contao Online Bundle
+ * @link       https://github.com/BugBuster1701/contao-online-bundle
+ *
  * @license    LGPL-3.0-or-later
- * @see        https://github.com/BugBuster1701/contao-online-bundle
  */
 
-/**
+/*
  * Table tl_online_session
  */
-$GLOBALS['TL_DCA']['tl_online_session'] = [
-
+$GLOBALS['TL_DCA']['tl_online_session'] = array(
 	// Config
-	'config' => [
-		'sql' => [
-			'keys' => [
+	'config' => array(
+		'sql' => array(
+			'keys' => array(
 				'id' => 'primary',
 				'pid' => 'index'
-			],
-		],
-	],
+			),
+		),
+	),
 
 	// Fields
-	'fields' => [
-		'id' => [
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'autoincrement' => true]
-		],
-		'pid' => [
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0]
-		],
-		'tstamp' => [
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0]
-		],
-		'instanceof' => [
-			'sql' => ['type' => 'string', 'length' => 255, 'default' => '']
-		],
-		'loginhash' => [
-			'sql' => ['type' => 'string', 'length' => 64, 'notnull' => false]
-		],
-	],
-];
+	'fields' => array(
+		'id' => array(
+			'sql' => array('type' => 'integer', 'unsigned' => true, 'autoincrement' => true)
+		),
+		'pid' => array(
+			'sql' => array('type' => 'integer', 'unsigned' => true, 'default' => 0)
+		),
+		'tstamp' => array(
+			'sql' => array('type' => 'integer', 'unsigned' => true, 'default' => 0)
+		),
+		'instanceof' => array(
+			'sql' => array('type' => 'string', 'length' => 255, 'default' => '')
+		),
+		'loginhash' => array(
+			'sql' => array('type' => 'string', 'length' => 64, 'notnull' => false)
+		),
+	),
+);
