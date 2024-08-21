a1aa9f0ab330e715b999922af9969cf6f300cd0c
Glen Langer <contao.modul@glen-langer.de>
2024-08-22 00:35:53 +0200

    #15, Umbau Struktur

--- ecs.php
+++ ecs.php
@@ -18,21 +18,20 @@ declare(strict_types=1);
 use Contao\EasyCodingStandard\Set\SetList;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return ECSConfig::configure()
     ->withSets([SetList::CONTAO])
     // Adjust the configuration according to your needs.
-    // ->withFileExtensions(['php'])
+    ->withFileExtensions(['php'])
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/ecs.php',
-    ])
-    ->withSkip([
-        'src/Resources/contao/*',
     ])
     ->withConfiguredRule(HeaderCommentFixer::class, [
         'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Contao Online Bundle\n@link       https://github.com/BugBuster1701/contao-online-bundle\n\n@license    LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.",
     ])
     ->withParallel()
+    ->withSpacing(Option::INDENTATION_SPACES, "\n")
     ->withCache(sys_get_temp_dir().'/ecs/ecs')
 ;
