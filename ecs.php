5f0d31128c544cb38d8197e15a1c39f6205d9734
Glen Langer <contao.modul@glen-langer.de>
2024-08-22 01:11:41 +0200

    #15, Anpassungen ecs.php und erste fixe

--- ecs.php
+++ ecs.php
@@ -16,13 +16,14 @@ declare(strict_types=1);
  */
 
 use Contao\EasyCodingStandard\Set\SetList;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return ECSConfig::configure()
     ->withSets([SetList::CONTAO])
-    // Adjust the configuration according to your needs.
     ->withPaths([
         __DIR__.'/contao',
         __DIR__.'/src',
@@ -30,7 +31,17 @@ return ECSConfig::configure()
     ])
     ->withConfiguredRule(HeaderCommentFixer::class, [
         'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Contao Online Bundle\n@link       https://github.com/BugBuster1701/contao-online-bundle\n\n@license    LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.",
+        'comment_type' => 'comment',
+    ])
+    ->withSkip([
+        DeclareStrictTypesFixer::class => [
+            'contao/dca/tl_online_session.php',
+        ],
+        ArraySyntaxFixer::class => [
+            'contao/dca/tl_online_session.php',
+        ],
     ])
     ->withParallel()
+    ->withSpacing(Option::INDENTATION_SPACES, "\n")
     ->withCache(sys_get_temp_dir().'/ecs/ecs')
 ;
