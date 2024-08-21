f27de9321eca1af2eb44ba0ad0aa1c16e138ff76
Glen Langer <contao.modul@glen-langer.de>
2024-08-22 00:40:01 +0200

    #15, withPaths Anpassung

--- ecs.php
+++ ecs.php
@@ -23,8 +23,8 @@ use Symplify\EasyCodingStandard\ValueObject\Option;
 return ECSConfig::configure()
     ->withSets([SetList::CONTAO])
     // Adjust the configuration according to your needs.
-    ->withFileExtensions(['php'])
     ->withPaths([
+        __DIR__.'/contao',
         __DIR__.'/src',
         __DIR__.'/ecs.php',
     ])
@@ -32,6 +32,5 @@ return ECSConfig::configure()
         'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Contao Online Bundle\n@link       https://github.com/BugBuster1701/contao-online-bundle\n\n@license    LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.",
     ])
     ->withParallel()
-    ->withSpacing(Option::INDENTATION_SPACES, "\n")
     ->withCache(sys_get_temp_dir().'/ecs/ecs')
 ;
